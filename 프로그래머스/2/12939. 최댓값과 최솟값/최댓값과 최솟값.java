4a9ea644b8434ca3f3c211de805bbbd2dd93e9e4
Yerin Lee <91695537+lreowy@users.noreply.github.com>
2025-12-05 01:58:50 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 9.79 ms, Memory: 84.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,18 @@
+import java.util.StringTokenizer;
+
+class Solution {
+    public String solution(String s) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        StringTokenizer st = new StringTokenizer(s);
+        while(st.hasMoreTokens()){
+            int num = Integer.parseInt(st.nextToken());
+            if(num > max)
+                max = num;
+            if(min > num)
+                min = num;
+        }
+        String answer = min + " " + max;
+        return answer;
+    }
+}
