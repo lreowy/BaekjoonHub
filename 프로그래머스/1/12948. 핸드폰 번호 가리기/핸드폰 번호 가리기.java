a9b13e02a8be4462493ebaeb714c207ec5804697
Yerin Lee <91695537+lreowy@users.noreply.github.com>
2026-01-23 18:13:39 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 2.33 ms, Memory: 70.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,6 @@
+class Solution {
+    public String solution(String phone_number) {
+        String answer = phone_number.substring(0,phone_number.length()-4).replaceAll(".", "*") + phone_number.substring(phone_number.length()-4);
+        return answer;
+    }
+}
