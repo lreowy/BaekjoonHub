5e1a74928b2f24a3fa3d2553a9cda77bc9948bd8
Yerin Lee <91695537+lreowy@users.noreply.github.com>
2026-02-05 15:46:25 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 5.02 ms, Memory: 99.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static int solution(int left, int right) {
+        int answer = 0;
+        for(int i=left;i<=right;i++){
+            int count = 0;
+            for(int j=1;j<=i;j++){
+                if(i % j == 0)
+                    count++;
+            }
+            if(count % 2 == 1)
+                answer -= i;
+            else
+                answer += i;
+        }
+        return answer;
+    }
+}
