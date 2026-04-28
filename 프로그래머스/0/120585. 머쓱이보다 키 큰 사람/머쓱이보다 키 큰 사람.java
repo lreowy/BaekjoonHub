0f326e223891ebee312245489f2f73d7717db2d3
Yerin Lee <91695537+yxinot@users.noreply.github.com>
2026-04-28 20:05:48 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.02 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        for(int a : array){
+            if(height < a)
+                answer++;
+        }
+        return answer;
+    }
+}
